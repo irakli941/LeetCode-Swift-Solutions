a0f1d77723c604eafcb2756bf069c6c7a6fd0ee4
Irakli Shelia <iraklishelia941@gmail.com>
2024-01-05 15:13:59 +0400

    Updated solution for question #300

--- DynamicProgramming/LongestIncreasingSubsequence.swift
+++ DynamicProgramming/LongestIncreasingSubsequence.swift
@@ -14,27 +14,18 @@
  * Time Complexity: O(nlogn), Space Complexity: O(n^2)
  */
 
-class GuessNumberHigherOrLowerII {
-    func getMoneyAmount(_ n: Int) -> Int {
-        var table = [[Int]](repeatElement([Int](repeatElement(0, count: n + 1)), count: n + 1))
-        return DP(&table, 1, n)
-    }
-    
-    private func DP(_ table: inout [[Int]], _ s: Int, _ e: Int) -> Int {
-        if s >= e {
-            return 0
+class Solution {
+    func lengthOfLIS(_ nums: [Int]) -> Int {
+        var dp = Array(repeating: 1, count: nums.count)
+        for lastNumIndex in nums.indices {
+            var maxPrefixLength = 0
+            for prefixIndex in 0..<lastNumIndex {
+                if nums[lastNumIndex] > nums[prefixIndex] {
+                    maxPrefixLength = max(maxPrefixLength, dp[prefixIndex])
+                }
+            }
+            dp[lastNumIndex] = maxPrefixLength + 1
         }
-        if table[s][e] != 0 {
-            return table[s][e]
-        }
-        var guarantee = Int.max
-        for i in s..<e + 1 {
-            // To get the worse case of every choice to guarantee the result
-            let tmp = i + max(DP(&table, s, i - 1), DP(&table, i + 1, e))
-            // To get the least cost among the guaranteed results
-            guarantee = min(guarantee, tmp)
-        }
-        table[s][e] = guarantee
-        return guarantee
+        return dp.max() ?? 0
     }
 }
